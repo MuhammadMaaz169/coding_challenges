d93c7cb1cb00bbde81487b67fab1e78f7063f4d1
Muhammad Maaz <m.maaz169@gmail.com>
2024-08-17 14:54:54 +0500

    feat: auth middleware

--- chat-server-client/server/src/http_server/domains/auth/auth.route.js
+++ chat-server-client/server/src/http_server/domains/auth/auth.route.js
@@ -1,18 +1,19 @@
-import { login } from './auth.controller.js';
+import { auth } from '../auth.middleware.js';
+import { login, testAuthentication } from './auth.controller.js';
 
 export const authRoutes = (fastify, _, done) => {
-  const path = '/auth/login';
+  const path = '/auth';
 
   fastify.route({
     method: 'POST',
-    url: path,
+    url: path + '/login',
     schema: {
       tags: ['Auth'],
       body: {
         type: 'object',
         properties: {
-          email: { type: 'string' },
-          password: { type: 'string' },
+          email: { type: 'string', default: 'user1@gmail.com', },
+          password: { type: 'string', default: '1234',  },
         },
       },
       response: {
@@ -32,6 +33,31 @@ export const authRoutes = (fastify, _, done) => {
       },
     },
     handler: login,
+  });
+
+  fastify.route({
+    method: 'GET',
+    url: path + '/test',
+    schema: {
+      description: "Test authentication",
+      tags: ['Auth'],
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            success: { type: 'boolean' },
+            data: {
+              type: 'object',
+              properties: {
+                message: { type: 'string' },
+              },
+            },
+          },
+        },
+      },
+    },
+    preHandler: [auth],
+    handler: testAuthentication,
   });
   done();
 };
